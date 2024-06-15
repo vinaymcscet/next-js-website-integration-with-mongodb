5fe0e94139c43f83141a106cfd101e429c03c823
tarun294 <tarunrajput294@gmail.com>
2024-06-16 01:14:54 +0530

    change in contact module style path

--- src/app/components/MovieCard/page.js
+++ src/app/components/MovieCard/page.js
@@ -1,25 +1,32 @@
-import styles from '../../styles/common.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
-
+import styles from '@/app/styles/common.module.css'
+import Image from "next/image";
+import Link from "next/link";
 const MovieCard = (curElem) => {
-    const { id, type, title, synopsis, backgroundImage } = curElem.jawSummary || {};
+
+    const { id, type, title, synopsis } = curElem.jawSummary;
+
     return (
         <>
             <div className={styles.card}>
                 <div className={styles.card_image}>
-                    <Image src={backgroundImage.url ? backgroundImage.url : '/na.jpg'} alt={title || 'No Title'} width={260} height={200} />
+                    <Image src={curElem.jawSummary.backgroundImage.url} alt={title} width={260} height={200} />
                 </div>
                 <div className={styles.card_data}>
-                    <h2>{title.length > 1 ? title.substring(0, 18) : 'Movie Not available right now'}</h2>
-                    <p>{synopsis.length > 1 ? `${synopsis.substring(0, 66)}...` : 'Movie description not available'}</p>
+                    <h2>{title.substring(0, 18)}</h2>
+                    <p>
+                        {`${synopsis.substring(0, 66)} ...`}
+                    </p>
+
                     <Link href={`/movie/${id}`}>
-                        <button>Read More</button>
+                        <button>
+                            Read More
+                        </button>
                     </Link>
                 </div>
             </div>
-        </>
-    )
-}
 
-export default MovieCard
+        </>
+    );
+};
+
+export default MovieCard;
